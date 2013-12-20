c880dc8cf43bfe26c6b954b145f2ba216ff09d15
Cotil1ion <Cotil1ion@Cotil1ion-PC>
2013-12-20 20:36:04 +0200

    DAO interface,impl,factory,dto

---
+++ Social_Library/Social_Library-ejb/src/java/sociallibrary/dao/RoleDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sociallibrary.dao;
+
+import sociallibrary.dto.Role;
+/**
+ *
+ * @author Cotil1ion
+ */
+public interface RoleDAO {
+     public void insert(Role user);
+     public void update(Role user);
+     public void delete(int roleId);    
+}
---
+++ Social_Library/Social_Library-ejb/src/java/sociallibrary/daoimpl/RoleDAOImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sociallibrary.daoimpl;
+
+import sociallibrary.dao.RoleDAO;
+import sociallibrary.dto.Role;
+
+/**
+ *
+ * @author Cotil1ion
+ */
+public class RoleDAOImpl implements RoleDAO{
+
+    public void insert(Role user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void update(Role user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void delete(int roleId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
