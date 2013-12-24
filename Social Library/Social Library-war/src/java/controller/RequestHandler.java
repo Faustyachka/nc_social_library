9352d5e854d7dcdfd244e67abc29f08c70a8ca84
PavelBorisenko <PavelBorisenko@gmail.com>
2013-12-24 03:25:46 +0200

    controller

---
+++ Social Library/Social Library-war/src/java/controller/RequestHandler.java
@@ -0,0 +1,45 @@
+
+package Controller;
+
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+
+
+public class RequestHandler {
+    
+    private static RequestHandler instance=null;
+    HashMap <String, Command> commands = new HashMap<String,Command>();
+    
+public static RequestHandler getInstance(){
+    
+    if(instance==null){
+        instance= new RequestHandler();
+        return instance;
+    }
+    else{
+        return instance;
+    }
+        
+    }
+private RequestHandler(){
+    commands.put("rating", new ScoreCommand());
+    commands.put("nocommand", new NoCommand());
+    
+}
+
+public Command getCommand(HttpServletRequest request){
+     String action = request.getParameter("command");
+     
+     Command command = commands.get(action);
+      if(command==null){
+          
+          command =new NoCommand();
+      }
+    
+    return command;
+    
+    
+}
+    
+    
+}
