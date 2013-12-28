97edd3badb81f8c8dc74cb7d7856301e0c0a7b4f
Faustyachka <drfaustua@gmail.com>
2013-12-28 21:49:21 +0200

    add missing controller files

---
+++ Social_Library/Social_Library-war/src/java/controller/ScoreCommand.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author П
+ */
+class ScoreCommand implements Command {
+    
+  
+
+    public ScoreCommand () {
+    }
+
+    
+
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String page=null;
+            
+        //session bean (busines logic)!
+               
+        
+         page= ConfigurationManager.SCORE_PAGE;
+        return page;
+    }
+    
+}
